a8e8c915db836d8b931614b259fde263e8ecd893
laura-ksm <lara.nirvana67@gmail.com>
2021-05-09 20:31:56 -0400

    fetch api + hook example

--- hook-effect/src/App.js
+++ hook-effect/src/App.js
@@ -2,14 +2,27 @@ import React, { useState, useEffect } from 'react';
 
 function App() {
 
-  const [count, setCount] = useState(0);
+  const [resource, setResource] = useState('posts');
+  const [items, setItems] = useState([]);
 
   useEffect(() => {
-    document.title = `You clicked ${count} times`;
-  });
+    fetch(`https://jsonplaceholder.typicode.com/${resource}`)
+      .then(response => response.json())
+      .then(json => setItems(json))
+  }, [resource]);
 
   return (
-    <button onClick={(prev) => setCount(prev + 1)}>+</button>
+    <>
+      <div>
+        <button onClick={ () => setResource('posts') }>Posts</button>
+        <button onClick={ () => setResource('users') }>Users</button>
+        <button onClick={ () => setResource('comments') }>Comments</button>
+      </div>
+      <h2>{resource}</h2>
+      {items.map( (item, index) => {
+        return <div key={index}>{ JSON.stringify(item) }</div>
+      } )}
+    </>
   );
 }
 
